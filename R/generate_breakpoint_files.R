8bf3636289e8cf5ba838b9924de6906934694aaa
PollyHung <105984864+PollyHung@users.noreply.github.com>
2025-04-07 15:21:53 +0800

    banned the R code

--- R/generate_breakpoint_files.R
+++ R/generate_breakpoint_files.R
@@ -13,23 +13,23 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-loadCuts <- function(cuts.path) {
-  # Select interested columns
-  bpdf <- read.delim(cuts.path)
-  colnames(bpdf) <- tolower(colnames(bpdf))
-  bpdf <- bpdf %>% dplyr::select(chr, arm, cytoband, start, end, direction, telcent)
-  bpdf$arm <- ifelse(!grepl("p|q", bpdf$arm),
-                     paste0(bpdf$arm, gsub("[0-9]|\\.", "", bpdf$cytoband)),
-                     bpdf$arm)
-  bpdf$id <- paste0(bpdf$arm, "_", bpdf$telcent, "_", bpdf$direction)
-
-  return(bpdf)
-}
-
-
-files <- list.files("~/Desktop/BESAA/codes/BESAA/docs/breakpoints/", full.names = FALSE)
-for(i in files){
-  ref2 <- loadCuts(paste0("~/Desktop/BESAA/codes/BESAA/docs/breakpoints/", i))
-  i <- gsub(".txt", "", i)
-  save(ref2, file = paste0("data/", i, ".RData"))
-}
+# loadCuts <- function(cuts.path) {
+#   # Select interested columns
+#   bpdf <- read.delim(cuts.path)
+#   colnames(bpdf) <- tolower(colnames(bpdf))
+#   bpdf <- bpdf %>% dplyr::select(chr, arm, cytoband, start, end, direction, telcent)
+#   bpdf$arm <- ifelse(!grepl("p|q", bpdf$arm),
+#                      paste0(bpdf$arm, gsub("[0-9]|\\.", "", bpdf$cytoband)),
+#                      bpdf$arm)
+#   bpdf$id <- paste0(bpdf$arm, "_", bpdf$telcent, "_", bpdf$direction)
+#
+#   return(bpdf)
+# }
+#
+#
+# files <- list.files("~/Desktop/BESAA/codes/BESAA/docs/breakpoints/", full.names = FALSE)
+# for(i in files){
+#   ref2 <- loadCuts(paste0("~/Desktop/BESAA/codes/BESAA/docs/breakpoints/", i))
+#   i <- gsub(".txt", "", i)
+#   save(ref2, file = paste0("data/", i, ".RData"))
+# }
