65803abb6071327f40f4b965cb058909f9203773
PollyHung <105984864+PollyHung@users.noreply.github.com>
2025-04-07 15:18:27 +0800

    added id in column

--- R/generate_breakpoint_files.R
+++ R/generate_breakpoint_files.R
@@ -17,6 +17,7 @@
 files <- list.files("~/Desktop/BESAA/codes/BESAA/docs/breakpoints/", full.names = FALSE)
 for(i in files){
   ref2 <- read.delim(paste0("~/Desktop/BESAA/codes/BESAA/docs/breakpoints/", i))
+  ref2 <- paste0(ref2$arm, "_", ref2$telcent, "_", ref2$direction)
   i <- gsub(".txt", "", i)
   save(ref2, file = paste0("data/", i, ".RData"))
 }
