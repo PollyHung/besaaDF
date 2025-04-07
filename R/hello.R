b7eb70992cd4e8f5dd57ec912dc18c62a7f2f28f
PollyHung <105984864+PollyHung@users.noreply.github.com>
2025-04-07 14:40:58 +0800

    Added the Breakpoint Files
    Added the Breakpoint Files

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -15,4 +15,10 @@
 
 hello <- function() {
   print("Hello, world!")
+}
+
+files <- list.files("~/Desktop/BESAA/codes/BESAA/docs/breakpoints/", full.names = FALSE)
+for(i in files){
+  df <- read.delim(paste0("~/Desktop/BESAA/codes/BESAA/docs/breakpoints/", i))
+  save(df, file = paste0("data/", i, ".RData"))
 }
